8bce3b93c2f5c6e1ac6dc94c7598f6237c55ee59
Zeerak Waseem <zeerak.w@gmail.com>
2017-03-13 14:55:17 +0000

    Move to argparse, ensure files + dirs exist, fix indentation

--- build_brown_clusters.py
+++ build_brown_clusters.py
@@ -3,38 +3,38 @@ import os, sys, getopt, subprocess, gzip, json, argparse
 from GZIPTweetStream import GZIPTweetStream
 
 def run_brown_clustering(input_file, c, out_dir):
-	c = 5
+    c = 5
 
-	#Create temporary directories to store input/output files
-	in_dir = "./input"
+    #Create temporary directories to store input/output files
+    in_dir = "./input"
 
-	try:
-		os.makedirs(in_dir)
-	except Exception, e:
-		print "Didnt create temp input dir (",str(e),")"
+    try:
+        os.makedirs(in_dir)
+    except Exception, e:
+        print "Didnt create temp input dir (",str(e),")"
 
-	try:
-		os.makedirs(out_dir)
-	except Exception, e:
-		print "Didnt create temp output dir (",str(e),")"
+    try:
+        os.makedirs(out_dir)
+    except Exception, e:
+        print "Didnt create temp output dir (",str(e),")"
 
 
-	#extract text from gzipped json file
-	#write texts to temp file, tokens separated by spaces, one text per line
-	stream = GZIPTweetStream([input_file])
-	in_fn = "inputfile"
-	with open(in_dir+"/"+in_fn,"w+") as f:
-		for tweet in stream:
-			f.write(" ".join(tweet).encode("utf-8")+"\n")
+    #extract text from gzipped json file
+    #write texts to temp file, tokens separated by spaces, one text per line
+    stream = GZIPTweetStream([input_file])
+    in_fn = "inputfile"
+    with open(in_dir+"/"+in_fn,"w+") as f:
+        for tweet in stream:
+            f.write(" ".join(tweet).encode("utf-8")+"\n")
 
-	cluster_command = "./brown-cluster/wcluster"
-	args = [cluster_command,
-			"--text", in_dir+"/"+in_fn,
-			"--c", str(c),
-			"--output_dir", out_dir
-		]
+    cluster_command = "./brown-cluster/wcluster"
+    args = [cluster_command,
+            "--text", in_dir+"/"+in_fn,
+            "--c", str(c),
+            "--output_dir", out_dir
+        ]
 
-	code = subprocess.call(args)
+    code = subprocess.call(args)
 
 # Deprecated
 # def usage():
@@ -68,21 +68,21 @@ def run_brown_clustering(input_file, c, out_dir):
 # 		usage()
 
 if __name__ == "__main__":
-        parser = argparse.ArgumentParser(
-        description = """
-        Usage:
-        "build_brown_clusters.py -i <input_file> -c <num_clusters> -o <output_directory>"
-        """)
+    parser = argparse.ArgumentParser(
+    description = """
+    Usage:
+    "build_brown_clusters.py -i <input_file> -c <num_clusters> -o <output_directory>"
+    """)
 
-        parser.add_argument('-i', '--input', help = "Input file")
-        parser.add_argument('-c', '--clusters', help = "Number of clusters")
-        parser.add_argument('-o', '--out_dir', help = "Output directory")
-        args = parser.parse_args()
+    parser.add_argument('-i', '--input', help = "Input file")
+    parser.add_argument('-c', '--clusters', help = "Number of clusters")
+    parser.add_argument('-o', '--out_dir', help = "Output directory")
+    args = parser.parse_args()
 
-        while not os.path.exists(args.input):
-            args.input = input("The file does not exist. Please provide me with the actual file")
+    while not os.path.exists(args.input):
+        args.input = input("The file does not exist. Please provide me with the actual file")
 
-        if not os.path.exists(args.out_dir):
-            os.makedirs(args.out_dir)
+    if not os.path.exists(args.out_dir):
+        os.makedirs(args.out_dir)
 
-        run_brown_clustering(args.input, args.clusters, args.out_dir)
+    run_brown_clustering(args.input, args.clusters, args.out_dir)
